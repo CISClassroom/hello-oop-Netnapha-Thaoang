e420850e6bff8a4815ca2ed64bc8de6d696c0762
Netnapha-Thaoang <netnapha_th@kkumail.com>
2020-03-07 12:29:37 +0700

    test

---
+++ src/com/company/Lab1.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Lab1{
+    public static void main(String[] args) {
+	System.out.println("Hello Java");
+	Student st = new Student();
+	st.GetStudent();
+    }
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Student {
+    String name = "Netnapha";
+    String id = "623410069-8";
+    int age = 2563-2544;
+    public void GetStudent(){
+        System.out.println("ชื่อ : " +name);
+        System.out.println("รหัสนักศึกษา : " +id);
+        System.out.println(age);
+        System.out.println("สาขาวิทยาการคอมพิวเตอร์และสารสนเทศ");
+    }
+}
